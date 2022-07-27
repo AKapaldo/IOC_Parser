0847fd756ffefbd1502e0880cc596d82ab73e16e
Andrew Kapaldo <53104260+AKapaldo@users.noreply.github.com>
2022-07-27 10:44:43 -0400

    Update IOC_Parser.py

--- IOC_Parser.py
+++ IOC_Parser.py
@@ -7,7 +7,7 @@ This program is used for parsing IOC notices to run all time queries in Splunk.
 __author__ = "Andrew Kapaldo"
 __copyright__ = "Copyright 2022"
 __license__ = "GPL"
-__version__ = "1.0"
+__version__ = "1.2"
 __status__ = "Development"
 
 import re
@@ -27,16 +27,19 @@ def main():
 
     #Variables for program
     global final
+    global email_final
     start = "**************** INDICATORS OF COMPROMISE"
     end = "********************"
     query = ""
+    email_final = ""
     qstart = "earliest=1 latest=+10y ("
     qend = ")"
     terms = {
         "net": [],
         "hashes": [],
         "sites": [],
-        "docs": []
+        "docs": [],
+        "eaddress": []
     } 
 
 
@@ -65,11 +68,20 @@ def main():
     #Get Files
     files = re.findall(r'^(.*\.(?:doc|docx|docm|rtf|ppt|pptx|xls|xlsx|xlsm|xml|pdf|7z|zip|exe|msi|msp|iso|py|ps1|psm1|bat|cmd|sh|jar|js|vb))$', content, re.MULTILINE)
 
+    #Get Email Addresses
+    email = re.findall(r'([\w\.-]+@[\w\.-]+(?:\.[\w]+)+)', content)
+
     #See if IP is in URL list
     urls2 = [x for x in urls if x not in ips]
 
     #See if Docs in URL List
     site = [x for x in urls2 if x not in files]
+
+    #Remove any iJC3 emails
+    if 'DL-IM-JC3-Analytics@hq.doe.gov'in email:
+        email.remove("DL-IM-JC3-Analytics@hq.doe.gov")
+    if 'circ@jc3.doe.gov' in email:
+        email.remove("circ@jc3.doe.gov")
 
     #Add URLs to dictionary
     for each in site:
@@ -92,6 +104,10 @@ def main():
     for doc in files:
         terms["docs"].append(doc)
 
+    #Add emails to dictionary
+    for address in email:
+        terms["eaddress"].append(address)
+
     #Modify to add the search terms
     if len(terms["net"]) > 0:
         for y in range(len(terms["net"])):
@@ -109,25 +125,50 @@ def main():
         for y in range(len(terms["sites"])):
             query += "\"" + terms["sites"][y] + "\"" + " OR "
 
+    if len(terms["eaddress"]) > 0:
+        for y in range(len(terms["eaddress"])):
+            email_final += terms["eaddress"][y] + "; "
+
     #Remove the last " OR " and add the query start and end
     query = query[:-4]
     final = qstart + query + qend
 
+    #Remove the last "; " for emails
+    if email_final:
+        email_final = email_final[:-2]
+    else:
+        email_final = "No email addresses listed in IOCs"
+
     #GUI Text box and contents
     text_box = tk.Text(
-        results_frame,
-        height=25,
+        splunk_frame,
+        height=20,
         width=100
     )
+
+    email_box = tk.Text(
+    email_frame,
+    height=3,
+    width=100
+    )
+
     text_box.place(x=10,y=10)
     text_box.insert('end', final)
     text_box.config(state='normal')
 
-#Clear the clipboard and copy the result
+    email_box.place(x=10,y=10)
+    email_box.insert('end', email_final)
+    email_box.config(state='normal')
+
+#Clear the clipboard and copy the Splunk result
 def copy_to_clipboard():
     window.clipboard_clear()
     window.clipboard_append(final.rstrip())
 
+#Clear the clipboard and copy the email result
+def email_to_clipboard():
+    window.clipboard_clear()
+    window.clipboard_append(email_final.rstrip())
 
 #Configure GUI Window
 window = tk.Tk()
@@ -136,17 +177,31 @@ window.geometry('850x600')
 window.config(bg='#202124')
 
 
-results_frame = ttk.LabelFrame(window, text='Results')
-results_frame.place(x=10, y=70, height=450, width=830)
+splunk_frame = ttk.LabelFrame(window, text='Splunk Query')
+splunk_frame.place(x=10, y=70, height=450, width=830)
+
+email_frame = ttk.LabelFrame(window, text='O365 Email Query - Sender')
+email_frame.place(x=10, y=430, height=100, width=830)
 
 text_box = tk.Text(
-        results_frame,
-        height=25,
+        splunk_frame,
+        height=20,
         width=100
     )
+
+email_box = tk.Text(
+    email_frame,
+    height=3,
+    width=100
+)
+
 text_box.place(x=10,y=10)
 text_box.insert('end', "")
 text_box.config(state='normal')
+
+email_box.place(x=10,y=10)
+email_box.insert('end', "")
+email_box.config(state='normal')
 
 #GUI Label and open dialog for file selection
 label = tk.Label(text="Click \"Open...\" to select a file to parse for IOCs...", font=("Helvetica", 14), background="#202124", foreground="#dddddd", pady=20)
@@ -156,12 +211,15 @@ button_open = tk.Button(window, border=4 ,text="Open...", bg='#dddddd',command=m
 button_open.place(x=20, y=550)
 
 #GUI Copy button
-button_copy = tk.Button(window, border=4 ,text="Copy To Clipboard", bg='#dddddd',command=copy_to_clipboard)
+button_copy = tk.Button(window, border=4 ,text="Splunk To Clipboard", bg='#dddddd',command=copy_to_clipboard)
 button_copy.place(x=80, y=550)
+
+button_copy = tk.Button(window, border=4 ,text="Email To Clipboard", bg='#dddddd',command=email_to_clipboard)
+button_copy.place(x=210, y=550)
 
 #GUI Exit button
 button_quit = tk.Button(window, border=4 ,text="Exit", bg='#dddddd',command=lambda: window.quit())
-button_quit.place(x=200, y=550)
+button_quit.place(x=330, y=550)
 
 
 #Call the Main Loop
